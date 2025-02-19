cc8f9534901a34f94d1e43d00702dd966b44bb4c
DESKTOP-K3TJ618\user <wooyeong1998@gmail.com>
2025-02-19 12:11:57 +0900

    [CHORE] Eclipse Project transfer to Intelij

--- workspace/ch03/src/OperatorEx06.java
+++ workspace/ch03/src/OperatorEx06.java
@@ -2,7 +2,7 @@ class OperatorEx06 {
       public static void main(String[] args) { 
             byte a = 10; 
             byte b = 20; 
-            byte c = a + b; 
+            byte c = (byte)(a + b);
 
             System.out.println(c); 
       } 
